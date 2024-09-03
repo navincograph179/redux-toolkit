83a5362cb18bcdb684e65679f287a8f6ad90bf47
navincograph179 <navin_gurung@co-graph.com>
2024-09-03 13:18:29 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
-
-function App() {
+import { decrement, increment } from './redux/counterSlice';
+import { useSelector } from 'react-redux';
+const App = () => {
+  const count = useSelector((state) => state.counter.value)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Count : {count}</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/redux/Store.js
@@ -0,0 +1,9 @@
+import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "./counterSlice"
+
+
+export const Store = configureStore({
+    reducer: {
+      counter : counterReducer,
+    },
+})
