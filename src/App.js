5105cef80c7fe0c965d2f7179c663f78d1e7bd9f
navincograph179 <navin_gurung@co-graph.com>
2024-09-03 13:34:17 +0900

    redux toolkit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
-import React from 'react'
-import './App.css';
-import { decrement, increment } from './redux/counterSlice';
-import { useSelector } from 'react-redux';
-const App = () => {
-  const count = useSelector((state) => state.counter.value)
+//useSelectorはアクセス, useDipatchで更新
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import "./App.css";
+import { decrement, increment, incrementByAmount } from "./redux/counterSlice";
+
+function App() {
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+
+  const [incrementAmount, setIncrementAmount] = useState("2");
+
   return (
-    <div className='App'>
-      <h1>Count : {count}</h1>
+    <div className="App">
+      <h1>Count: {count}</h1>
+      <input
+        value={incrementAmount}
+        onChange={(e) => setIncrementAmount(e.target.value)}
+      />
+      <button onClick={() => dispatch(increment())}>＋</button>
+      <button onClick={() => dispatch(decrement())}>ー</button>
+      <button
+        onClick={() => dispatch(incrementByAmount(Number(incrementAmount)))}
+      >
+        追加
+      </button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
